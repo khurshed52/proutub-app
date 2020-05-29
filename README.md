01541f45e125a557a378b75f7ad2d8bbba5763f6
khurshed52 <khurshedkhan52@gmai.com>
2020-05-30 01:18:15 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# proutub-app
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,18 @@ export class Tab1Page {
 
   constructor() {}
 
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay:true,
+    pagination: true
+  } 
+
+ images = [
+   {id: 1, url:'../../assets/images/slide-1.svg', text: 'Grow Your YouTube Channel with Genuine Views, Likes, Subscribers and Comments.'},
+   {id: 2, url:'../../assets/images/slide-2.svg', text: 'Get More Genuine Facebook Likes, Increase Facebook Likes, Post Likes, Profile Followers'},
+   {id: 3, url:'../../assets/images/slide-3.svg', text: 'Get More Likes and views on Your Instagram'}
+ ]
+
+
 }
